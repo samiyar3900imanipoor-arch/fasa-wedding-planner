385c3e080066c04adec9c0db9a586ef6568f9506
samiyar3900imanipoor-arch <samiyar3900imanipoor@gmail.com>
2026-06-19 01:53:52 +0330

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <h1>ROOT TEST 🚀</h1>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 )
